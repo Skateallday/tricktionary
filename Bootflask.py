e6c161cdbbad677387c6b188f48e98ec5b03f02e
Marc Inglis <marcinglis91@hotmail.com>
2018-10-13 23:27:39 +0100

    New file editions

--- Bootflask.py
+++ Bootflask.py
@@ -1,7 +1,9 @@
 from flask import Flask, flash, render_template, request
 import json
+
 with open('static/skaters.json') as f:
 	data = json.load(f)
+
 	
 app = Flask(__name__)
 
@@ -11,13 +13,14 @@ def root():
 		skater=skater
 	return render_template("Assignment.html",  skater=skater)
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/Search/', methods=['POST','GET'])
 def my_form_post():
 	if request.method == 'POST':
 		print request.form
 		search = request.form['search']
 		
 	return render_template("Search.html", search=search)
+
 
 @app.route('/about/')
 def about():
@@ -34,11 +37,14 @@ def home():
 def contact():
 	return render_template("contact.html")
 
+@app.route('/results/')
+def results():
+	return render_template("results.html")
 
-@app.route('/Search/',  methods=['GET', 'POST'])
-def search():
+@app.route('/results/<stat>')
+def stat(stat):
+	return "These are your results %s" %stat
+	return render_template("results.html")
 
-	return render_template("Search.html", search=search)
-	
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
