74e96f4870328f2ac2bc2ac53ac7fdd0c4052bf4
Marc Inglis <marcinglis91@hotmail.com>
2018-10-15 11:23:30 +0100

    Newly edited files

--- Bootflask.py
+++ Bootflask.py
@@ -3,6 +3,7 @@ import json
 
 with open('static/skaters.json') as f:
 	data = json.load(f)
+	print(json.dumps(data, indent=2))
 
 	
 app = Flask(__name__)
@@ -10,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def root():
 	for skater in data['skaters']:
-		skater=skater
+		print(skater) 
 	return render_template("Assignment.html",  skater=skater)
 
 @app.route('/Search/', methods=['POST','GET'])
@@ -18,8 +19,13 @@ def my_form_post():
 	if request.method == 'POST':
 		print request.form
 		search = request.form['search']
+	for skater in data['skaters']:
+		if search in skater:
+			return skater
+		return None	
+	print my_form_post(skaters, search)	
 		
-	return render_template("Search.html", search=search)
+	return render_template("Search.html", search=search, skater=skater)
 
 
 @app.route('/about/')
@@ -29,7 +35,7 @@ def about():
 @app.route('/home/')
 def home():
 	for skater in data['skaters']:
-		skater=skater
+		print(skater)
 	
 	return render_template("Assignment.html", skater=skater)
 
@@ -37,14 +43,19 @@ def home():
 def contact():
 	return render_template("contact.html")
 
-@app.route('/results/')
-def results():
-	return render_template("results.html")
+@app.route('/results/<string:stat>')
+def results(stat):
+	#for skater in data['skaters']:
+		#print(skater)
+	return render_template("results.html",  stat=stat)
 
-@app.route('/results/<stat>')
-def stat(stat):
-	return "These are your results %s" %stat
-	return render_template("results.html")
-
+@app.route('/upload/', methods=['POST', 'GET'])
+def upload():
+	if request.method == 'POST':
+		print request.form
+		upload = request.form['upload']
+		with open('static/newSkaters.json', 'w') as G:
+			json.dump(request.form, G)
+		return render_template("upload.html", upload=upload)
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
